b9ee5585746359925275c185e65f8300738d5d05
unknown <subaru4ten@gmail.com>
2022-01-05 01:29:54 +0100

    updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,9 @@ const Header = () => {
   return (
     <>
       <div
-        className=" flex items-center justify-center "
+        className=" flex items-center justify-center  "
         style={{
-          padding: '42px 38px 29px 38px',
-          margin: '20px 20%',
-          border: '2px solid #f5f5f5'
+          padding: '42px 38px 29px 38px'
         }}
       >
         Generated Contact:
